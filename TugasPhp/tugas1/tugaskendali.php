839c97045c7c30795a0b4b3fbaff1f38c3138e23
JEKII <achmadzaki2804@gmail.com>
2025-10-11 16:48:37 +0700

    mengganti tugas1php

--- TugasPhp/tugas1/tugaskendali.php
+++ TugasPhp/tugas1/tugaskendali.php
@@ -1,9 +1,23 @@
-<?php
-$nilai = 80; // ganti nilai ini untuk coba
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Kendali - Ternary</title>
+</head>
+<body>
+    <?php
+    // variabel
+    $nama = "Achmad Zaki";
+    $nilai = 75;
 
-if ($nilai > 70) {
-    echo "Lulus";
-} else {
-    echo "Remedial";
-}
-?>
+    // struktur kendali dengan ternary
+    $keterangan = ($nilai > 70) ? "Lulus" : "Remedial";
+    ?>
+
+    <h2>Hasil Penilaian</h2>
+    Nama Siswa: <?= $nama ?> <br>
+    Nilai: <?= $nilai ?> <br>
+    Keterangan: <?= $keterangan ?>
+</body>
+</html>
